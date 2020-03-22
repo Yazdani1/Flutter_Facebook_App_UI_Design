7e0bad642fe4857e5e90225a573c89059f3f3513
Yazdani <yaz4dani@gmail.com>
2020-03-22 22:31:51 +0600

    Story Option

--- lib/Home.dart
+++ lib/Home.dart
@@ -156,7 +156,6 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         //end of second container
-
                         Container(
                           height: 30.0,
                           width: 0.5,
@@ -380,12 +379,12 @@ class _HomeState extends State<Home> {
                                 ],
                               ),
                             )
-
-
-
                           ],
                         ),
                       ),
+
+
+
                     ],
                   ),
                 ),
